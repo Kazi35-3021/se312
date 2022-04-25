fb2d64dff4ba8e54b0294861ae5e3787dddaa65d
Kazi35-3021 <86186589+Kazi35-3021@users.noreply.github.com>
2022-04-25 15:30:55 +0600

    add file

---
+++ fxm.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+ int main()
+
+ {
+     char str1[30]= “google”;
+
+char str2[30]= “Google scholar ”;
+
+char str3[30]= “GOOGLE”;
+
+Char str4[30];
+
+strcat(str1,str2)
+
+puts(str1);
+
+d=strlen(str1);
+
+printf (“Both String are same \n”);
+
+puts(strlwr(str1));
+
+puts(strupr(str2));
+
+strncpy(str4,str2);
+
+puts(str4);
+
+printf(“you have successfully complete the program” \n);
+
+puts(strrev(str2))
+return 0
+}
